c72f3fe94654eceb3938570b4f68ae7d00fc541f
chaoming <adaduo@outlook.com>
2020-10-21 11:42:48 +0800

    change 'run()' to NeuType

--- npbrain/core_system/neuron_group.py
+++ npbrain/core_system/neuron_group.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import inspect
+from copy import deepcopy
+
 from .base_objects import BaseEnsemble
 from .base_objects import BaseType
 from .base_objects import ModelUseError
+from .base_objects import NeuState
 from .base_objects import _NEU_GROUP
 from .. import numpy as np
+from .. import profile
+from ..tools import DictPlus
 
 __all__ = [
     'NeuType',
@@ -24,8 +30,58 @@ class NeuType(BaseType):
         super(NeuType, self).__init__(requires=requires, steps=steps, name=name, vector_based=vector_based,
                                       heter_params_replace=heter_params_replace)
 
-    def first_try(self):
-        pass
+    def run(self, duration, monitors, vars_init=None):
+        # times
+        if isinstance(duration, (int, float)):
+            start, end = 0, duration
+        elif isinstance(duration, (tuple, list)):
+            assert len(duration) == 2, 'Only support duration with the format of "(start, end)".'
+            start, end = duration
+        else:
+            raise ValueError(f'Unknown duration type: {type(duration)}')
+        dt = profile.get_dt()
+        times = np.arange(start, end, dt)
+
+        # monitors
+        mon = DictPlus()
+        for k in monitors:
+            mon[k] = np.zeros(len(times))
+        mon['ts'] = times
+
+        # variables
+        variables = deepcopy(self.variables)
+        if vars_init is not None:
+            assert isinstance(vars_init, dict)
+            for k, v in vars_init.items():
+                variables[k] = v
+
+        # get update function
+        update = self.steps[0]
+        assert callable(update)
+
+        # initialize corresponding state
+        ST = NeuState(variables)(1)
+
+        # get the running _code
+        code_scope = {'update': update, 'monitor': mon, 'ST': ST,
+                      'mon_keys': monitors, 'dt': dt, 'times': times}
+        code_args = inspect.getfullargspec(update).args
+        mapping = {'ST': 'ST', '_t_': 't', '_i_': 'i', '_dt_': 'dt'}
+        code_arg2call = [mapping[arg] for arg in code_args]
+        code_lines = [f'def run_func():']
+        code_lines.append(f'  for i, t in enumerate(times):')
+        code_lines.append(f'    update({", ".join(code_arg2call)})')
+        code_lines.append(f'    for k in mon_keys:')
+        if self.vector_based:
+            code_lines.append(f'      monitor[k][i] = ST[k][0]')
+        else:
+            code_lines.append(f'      monitor[k][i] = ST[k]')
+
+        # run the model
+        codes = '\n'.join(code_lines)
+        exec(compile(codes, '', 'exec'), code_scope)
+        code_scope['run_func']()
+        return mon
 
 
 class NeuGroup(BaseEnsemble):
