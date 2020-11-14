30f608c69de4647c5ae4ce545c2c07f7114fb80d
chaoming <oujago@hotmail.com>
2020-11-14 23:40:29 +0800

    Modify synapse examples

--- examples/synapses/ordinary_synapses.py
+++ examples/synapses/ordinary_synapses.py
@@ -4,8 +4,8 @@ import brainpy as bp
 import brainpy.numpy as np
 
 
-def VoltageJumpSynapse():
-    """Voltage jump synapses.
+def VoltageJumpSynapse_with_ref():
+    """Voltage jump synapses with post-synaptic neuron refractory.
 
     .. math::
 
@@ -19,16 +19,52 @@ def VoltageJumpSynapse():
         pre2post=bp.types.ListConn(),
     )
 
-    def update(ST, pre, pre2post):
-        s = np.zeros_like(ST['s'], dtype=np.float_)
-        for pre_id in np.where(pre['sp'] > 0)[0]:
-            post_ids = pre2post[pre_id]
-            s[post_ids] = 1
+    def update(ST, pre, post, pre2post):
+        num_post = post['V'].shape[0]
+        s = np.zeros_like(num_post, dtype=np.float_)
+        for pre_id in range(pre['sp'].shape[0]):
+            if pre['sp'][pre_id] > 0.:
+                post_ids = pre2post[pre_id]
+                s[post_ids] = 1.
         ST['s'] = s
 
     @bp.delayed
     def output(ST, post):
         post['V'] += ST['s'] * post['not_ref']
+
+    return bp.SynType(name='VoltageJumpSynapse',
+                      requires=requires,
+                      steps=(update, output),
+                      vector_based=True)
+
+
+def VoltageJumpSynapse_without_ref():
+    """Voltage jump synapses without post-synaptic neuron refractory.
+
+    .. math::
+
+        I_{syn} = \sum J \delta(t-t_j)
+    """
+
+    requires = dict(
+        ST=bp.types.SynState(['s']),
+        pre=bp.types.NeuState(['sp']),
+        post=bp.types.NeuState(['V']),
+        pre2post=bp.types.ListConn(),
+    )
+
+    def update(ST, pre, post, pre2post):
+        num_post = post['V'].shape[0]
+        s = np.zeros_like(num_post, dtype=np.float_)
+        for pre_id in range(pre['sp'].shape[0]):
+            if pre['sp'][pre_id] > 0.:
+                post_ids = pre2post[pre_id]
+                s[post_ids] = 1.
+        ST['s'] = s
+
+    @bp.delayed
+    def output(ST, post):
+        post['V'] += ST['s']
 
     return bp.SynType(name='VoltageJumpSynapse',
                       requires=requires,
