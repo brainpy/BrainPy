825771e47d0288b293de4f054d29f71b44bb79e1
Chaoming Wang <adaduo@outlook.com>
2021-01-07 22:42:35 +0800

    "inputs" error notify

--- brainpy/core/network.py
+++ brainpy/core/network.py
@@ -139,7 +139,8 @@ class Network(object):
                 else:
                     data_type = 'fix'
             else:
-                raise ModelUseError('For each input, input[2] must be a numerical value to specify input values.')
+                raise ModelUseError(f'For each input, input[2] must be a numerical value to '
+                                    f'specify input values, but we get a {type(inp)}')
 
             # operation
             if len(inp) == 4:
@@ -179,9 +180,9 @@ class Network(object):
         exec(compile(func_code, '', 'exec'), code_scopes)
         step_func = code_scopes['step_func']
 
-        if profile._show_format_code:
+        if profile.show_format_code():
             tools.show_code_str(func_code.replace('def ', f'def network_'))
-        if profile._show_code_scope:
+        if profile.show_format_code():
             tools.show_code_scope(code_scopes, ['__builtins__', 'step_func'])
 
         return step_func
@@ -208,7 +209,8 @@ class Network(object):
         if isinstance(duration, (int, float)):
             start, end = 0, duration
         elif isinstance(duration, (tuple, list)):
-            assert len(duration) == 2, 'Only support duration with the format of "(start, end)".'
+            if len(duration) != 2:
+                raise ModelUseError('Only support duration with the format of "(start, end)".')
             start, end = duration
         else:
             raise ValueError(f'Unknown duration type: {type(duration)}')
