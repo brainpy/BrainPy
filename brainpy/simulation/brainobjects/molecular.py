1dff4df1f4c1cdad0dd51c243ed3b281c0b5bd7c
Chaoming Wang <adaduo@outlook.com>
2021-05-28 21:15:33 +0800

    new brain objects

---
+++ brainpy/simulation/brainobjects/molecular.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from brainpy.simulation.base import DynamicSystem
+
+__all__ = [
+    'Molecular'
+]
+
+
+class Molecular(DynamicSystem):
+    pass
+
---
+++ brainpy/simulation/brainobjects/soma.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from brainpy.simulation.base import DynamicSystem
+
+__all__ = [
+    'Soma'
+]
+
+
+class Soma(DynamicSystem):
+    pass
+
