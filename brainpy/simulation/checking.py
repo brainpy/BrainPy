0b26e41bedc8688dce380cf8d24cf70c2335a7af
Chaoming Wang <adaduo@outlook.com>
2021-07-04 22:05:45 +0800

    new unique name checking

--- brainpy/simulation/checking.py
+++ brainpy/simulation/checking.py
@@ -4,16 +4,27 @@ from brainpy import errors
 
 
 __all__ = [
-  'add'
+  'check_name',
+  'get_name',
 ]
 
 
 object2name = dict()
+_typed_names = {}
 
-
-def add(name, obj):
+def check_name(name, obj):
+  if not name.isidentifier():
+    raise errors.ModelUseError(f'"{name}" isn\'t a valid identifier '
+                               f'according to Python language definition. '
+                               f'Please choose another name.')
   if name in object2name:
     if id(object2name[name]) != id(obj):
       raise errors.UniqueNameError(name, object2name[name], obj)
 
 
+def get_name(type):
+  if type not in _typed_names:
+    _typed_names[type] = 0
+  name = f'{type}{_typed_names[type]}'
+  _typed_names[type] += 1
+  return name
