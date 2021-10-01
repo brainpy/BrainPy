784be025ac73242230e6ccc67eea59f905412ab8
chaoming <chao.brain@qq.com>
2021-10-01 12:23:57 +0800

    add base class for integrators

---
+++ brainpy/integrators/base.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+from brainpy.base.base import Base
+
+
+class Integrator(Base):
+  pass
+
+
---
+++ brainpy/integrators/fde/base.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from ..base import Integrator
+
+
+class FDEIntegrator(Integrator):
+  pass
+
