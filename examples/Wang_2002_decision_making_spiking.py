b48feff9e354286982191de0cf58697086b85397
chaoming <chao.brain@qq.com>
2021-12-03 11:59:55 +0800

    add several examples

---
+++ examples/Wang_2002_decision_making_spiking.py
@@ -0,0 +1,454 @@
+# -*- coding: utf-8 -*-
+
+# %% [markdown]
+# # *(Wang, 2002)* Decision making spiking model
+
+# %% [markdown]
+# Implementation of the paper: *Wang, Xiao-Jing. "Probabilistic decision making by slow reverberation in cortical circuits." Neuron 36.5 (2002): 955-968.*
+#
+# - Author : Chaoming Wang (chao.brain@qq.com)
+
+# %%
+import brainpy as bp
+import brainpy.math as bm
+
+bm.set_platform('cpu')
+import matplotlib.pyplot as plt
+
+
+# %% [markdown]
+# ## Neuron model
+
+# %% [markdown]
+# ### LIF neurons
+
+# %% [markdown]
+# Both pyramidal cells and interneurons are described by leaky integrate-and-fire neurons. 
+#
+# $$
+# C_{m} \frac{d V(t)}{d t}=-g_{L}\left(V(t)-V_{L}\right)-I_{s y n}(t)
+# $$
+#
+# where 
+#
+# - $I_{syn}(t)$ represents the total synaptic current flowing into the cell
+# - resting potential $V_L$ = -70 mV
+# - firing threshold $V_{th}$ = -50 mV
+# - reset potential $V_{rest}$ = -55 mV
+# - membrane capacitance $C_m$ = 0.5 nF for pyramidal cells and 0.2 nF for interneurons
+# - membrane leak conductance $g_L$ = 25 nS for pyramidal cells and 20 nS for interneurons
+# - refractory period $\tau_{ref}$ = 2 ms for pyramidal cells and 1 ms for interneurons
+
+# %%
+class LIF(bp.NeuGroup):
+  def __init__(self, size, V_L=-70., V_reset=-55., V_th=-50.,
+               Cm=0.5, gL=0.025, t_refractory=2., **kwargs):
+    super(LIF, self).__init__(size=size, **kwargs)
+
+    self.V_L = V_L
+    self.V_reset = V_reset
+    self.V_th = V_th
+    self.Cm = Cm
+    self.gL = gL
+    self.t_refractory = t_refractory
+
+    self.V = bm.Variable(bm.ones(self.num) * V_L)
+    self.input = bm.Variable(bm.zeros(self.num))
+    self.spike = bm.Variable(bm.zeros(self.num, dtype=bool))
+    self.refractory = bm.Variable(bm.zeros(self.num, dtype=bool))
+    self.t_last_spike = bm.Variable(bm.ones(self.num) * -1e7)
+
+    self.integral = bp.odeint(self.derivative)
+
+  def derivative(self, V, t, Iext):
+    dVdt = (- self.gL * (V - self.V_L) - Iext) / self.Cm
+    return dVdt
+
+  def update(self, _t, _dt):
+    ref = (_t - self.t_last_spike) <= self.t_refractory
+    V = self.integral(self.V, _t, self.input)
+    V = bm.where(ref, self.V, V)
+    spike = (V >= self.V_th)
+    self.V.value = bm.where(spike, self.V_reset, V)
+    self.spike.value = spike
+    self.t_last_spike.value = bm.where(spike, _t, self.t_last_spike)
+    self.refractory.value = bm.logical_or(spike, ref)
+    self.input[:] = 0.
+
+
+# %% [markdown]
+# ### Poisson neurons
+
+
+# %%
+class PoissonNoise(bp.NeuGroup):
+  def __init__(self, size, freq, **kwargs):
+    super(PoissonNoise, self).__init__(size=size, **kwargs)
+
+    self.freq = bm.Variable(bm.array([freq]))
+    self.dt = bm.get_dt() / 1000.
+    self.spike = bm.Variable(bm.zeros(self.num, dtype=bool))
+    self.rng = bm.random.RandomState()
+
+  def update(self, _t, _dt):
+    self.spike.value = self.rng.random(self.num) < self.freq[0] * self.dt
+
+
+# %%
+class PoissonStim(bp.NeuGroup):
+  def __init__(self, size, freq_mean, freq_var, t_interval, **kwargs):
+    super(PoissonStim, self).__init__(size=size, **kwargs)
+
+    self.freq_mean = freq_mean
+    self.freq_var = freq_var
+    self.t_interval = t_interval
+    self.dt = bm.get_dt() / 1000.
+
+    self.freq = bm.Variable(bm.zeros(1))
+    self.freq_t_last_change = bm.Variable(bm.ones(1) * -1e7)
+    self.spike = bm.Variable(bm.zeros(self.num, dtype=bool))
+    self.rng = bm.random.RandomState()
+
+  def update(self, _t, _dt):
+    in_interval = bm.logical_and(pre_stimulus_period < _t, _t < pre_stimulus_period + stimulus_period)
+    prev_freq = bm.where(in_interval, self.freq[0], 0.)
+    in_interval = bm.logical_and(in_interval, (_t - self.freq_t_last_change[0]) >= self.t_interval)
+    self.freq[:] = bm.where(in_interval, self.rng.normal(self.freq_mean, self.freq_var), prev_freq)
+    self.freq_t_last_change[:] = bm.where(in_interval, _t, self.freq_t_last_change[0])
+    self.spike.value = self.rng.random(self.num) < self.freq[0] * self.dt
+
+
+# %% [markdown]
+# ## Synapse models
+
+# %% [markdown]
+# The total synaptic currents are given by
+#
+# $$
+# I_{s y n}(t)=I_{e x t, A M P A}(t)+I_{r e c, A M P A}(t)+I_{r e c, N M D A}(t)+I_{r e c, G A B A}(t)
+# $$
+#
+# in which
+#
+# $$
+# \begin{gathered}
+# I_{\mathrm{ext}, \mathrm{AMPA}}(t)=g_{\mathrm{ext}, \mathrm{AMPA}}\left(V(t)-V_{E}\right) \mathrm{s}^{\mathrm{ext}, \mathrm{AMPA}}(t) \\
+# I_{\mathrm{rec}, \mathrm{AMPA}}(t)=g_{\mathrm{rec}, \mathrm{AMPA}}\left(V(t)-V_{E}\right) \sum_{\mathrm{j}=1}^{\mathrm{C}_{\mathrm{E}}} W_{j} S_{j}^{\mathrm{AMPA}}(t) \\
+# I_{\mathrm{rec}, \mathrm{NMDA}}(t)=\frac{g_{\mathrm{NMDA}}\left(V(t)-V_{E}\right)}{\left(1+\left[\mathrm{Mg}^{2+}\right] \exp (-0.062 V(t)) / 3.57\right)} \sum_{j=1}^{\mathrm{c}_{E}} w_{j} \mathrm{~s}_{j}^{\mathrm{NMDA}}(t) \\
+# I_{\mathrm{rec}, \mathrm{GABA}}(t)=g_{\mathrm{GABA}}\left(V(t)-V_{l}\right) \sum_{j=1}^{c_{1}} s_{j}^{\mathrm{GABA}}(t)
+# \end{gathered}
+# $$
+
+# %% [markdown]
+# where 
+#
+# - $V_E$ = 0 mV
+# - $V_I$ = -70 mV 
+# - $\left[\mathrm{Mg}^{2+}\right]$ = 1 mM
+# - The dimensionless weights $w_j$ represent the structured excitatory recurrent connections
+# - the sum over $j$ represents a sum over the synapses formed by presynaptic neurons $j$
+
+# %% [markdown]
+# ### AMPA
+#
+# The AMPA (external and recurrent) channels are described by
+#
+# $$
+# \frac{d s_{j}^{A M P A}(t)}{d t}=-\frac{s_{j}^{A M P A}(t)}{\tau_{A M P A}}+\sum_{k} \delta\left(t-t_{j}^{k}\right)
+# $$
+#
+# where 
+#
+# - the decay time of AMPA currents $\tau_{A M P A}$ = 2 ms
+# - for the external AMPA currents, the spikes are emitted according to a Poisson process with rate $V_{ext}$ = 2400 Hz independently from cell to cell
+
+
+# %%
+class AMPA(bp.TwoEndConn):
+  def __init__(self, pre, post, delay=0.5, g_max=0.10, E=0., tau=2.0, **kwargs):
+    super(AMPA, self).__init__(pre=pre, post=post, **kwargs)
+
+    # parameters
+    self.g_max = g_max
+    self.E = E
+    self.tau = tau
+    self.delay = delay
+
+    # variables
+    self.s = bm.Variable(bm.zeros(self.post.num))
+    self.pre_spike = self.register_constant_delay('ps', size=self.pre.num, delay=delay)
+
+    # function
+    self.integral = bp.odeint(self.derivative)
+
+  def derivative(self, s, t):
+    ds = - s / self.tau
+    return ds
+
+  def update(self, _t, _dt):
+    self.pre_spike.push(self.pre.spike)
+    pre_spike = self.pre_spike.pull()
+    self.s.value = self.integral(self.s, _t)
+    self.s += pre_spike.sum() * self.g_max
+    self.post.input += self.s * (self.post.V - self.E)
+
+
+# %%
+class AMPA_One(AMPA):
+  def update(self, _t, _dt):
+    self.pre_spike.push(self.pre.spike)
+    pre_spike = self.pre_spike.pull()
+    self.s.value = self.integral(self.s, _t)
+    self.s += pre_spike * self.g_max
+    self.post.input += self.s * (self.post.V - self.E)
+
+
+# %% [markdown]
+# ### NMDA
+
+# %% [markdown]
+# NMDA channels are described by:
+#
+# $$
+# \begin{gathered}
+# \frac{d s_{j}^{\mathrm{NMDA}}(t)}{d t}=-\frac{s_{j}^{\mathrm{NMDA}}(t)}{\tau_{\mathrm{NMDA}, \text { decay }}}+\alpha x_{j}(t)\left(1-s_{j}^{\mathrm{NMDA}}(t)\right) \\
+# \frac{d x_{j}(t)}{d t}=-\frac{x_{j}(t)}{\tau_{\mathrm{NMDA}, \text { rise }}}+\sum_{k} \delta\left(t-t_{j}^{k}\right)
+# \end{gathered}
+# $$
+#
+# where
+#
+# - the decay time $\tau_{\mathrm{NMDA}, \text { decay }}$ = 100 ms
+# - $\alpha$ = 0.5 $\mathrm{ms}^{-1}$
+# - the rise time $\tau_{\mathrm{NMDA}, \text { rise }}$ = 2 ms
+
+# %%
+class NMDA(bp.TwoEndConn):
+  def __init__(self, pre, post, delay=0.5, tau_decay=100, tau_rise=2.,
+               g_max=0.15, E=0., cc_Mg=1., alpha=0.5, **kwargs):
+    super(NMDA, self).__init__(pre=pre, post=post, **kwargs)
+
+    # parameters
+    self.g_max = g_max
+    self.E = E
+    self.cc_Mg = cc_Mg
+    self.alpha = alpha
+    self.tau_decay = tau_decay
+    self.tau_rise = tau_rise
+    self.delay = delay
+    self.size = (self.pre.num, self.post.num)
+    self.pre_one = bm.ones(self.pre.num)
+
+    # variables
+    self.pre_spike = self.register_constant_delay('ps', size=self.pre.num, delay=delay)
+    self.s = bm.Variable(bm.zeros(self.size))
+    self.x = bm.Variable(bm.zeros(self.size))
+
+    # function
+    self.integral = bp.odeint(self.derivative)
+
+  def derivative(self, s, x, t):
+    dsdt = -s / self.tau_decay + self.alpha * x * (1 - s)
+    dxdt = -x / self.tau_rise
+    return dsdt, dxdt
+
+  def update(self, _t, _dt):
+    self.pre_spike.push(self.pre.spike)
+    pre_spike = self.pre_spike.pull()
+    self.s.value, self.x.value = self.integral(self.s, self.x, _t)
+    self.x += pre_spike.reshape((-1, 1))
+    g_inf = 1 / (1 + self.cc_Mg * bm.exp(-0.062 * self.post.V) / 3.57)
+    Iext = bm.dot(self.pre_one, self.s) * (self.post.V - self.E) * g_inf
+    self.post.input += Iext * self.g_max
+
+
+# %% [markdown]
+# ### GABAA
+#
+# The GABA synaptic variable obeys
+#
+# $$
+# \frac{d s_{j}^{G A B A}(t)}{d t}=-\frac{s_{j}^{G A B A}(t)}{\tau_{G A B A}}+\sum_{k} \delta\left(t-t_{j}^{k}\right)
+# $$
+#
+# where
+# - the decay time of AMPA currents $\tau_{GABA}$ = 5 ms
+
+# %%
+class GABAa(AMPA):
+  def __init__(self, pre, post, delay=0.5, g_max=0.10, E=-70., tau=5.0, **kwargs):
+    super(GABAa, self).__init__(pre=pre, post=post, E=E, tau=tau, delay=delay, g_max=g_max, **kwargs)
+
+
+# %% [markdown]
+# ## Network model
+
+# %%
+class DecisionMaking(bp.Network):
+  def __init__(self, scale=1., mu0=40., coherence=25.6, **kwargs):
+    super(DecisionMaking, self).__init__(**kwargs)
+
+    num_exc = int(1600 * scale)
+    num_inh = int(400 * scale)
+    f = 0.15
+    num_A = int(f * num_exc)
+    num_B = int(f * num_exc)
+    num_N = num_exc - num_A - num_B
+    print(f'Total network size: {num_exc + num_inh}')
+
+    poisson_freq = 2400.  # Hz
+    w_pos = 1.7
+    w_neg = 1. - f * (w_pos - 1.) / (1. - f)
+    g_max_ext2E_AMPA = 2.1 * 1e-3  # uS
+    g_max_ext2I_AMPA = 1.62 * 1e-3  # uS
+    g_max_E2E_AMPA = 0.05 * 1e-3 / scale  # uS
+    g_max_E2E_NMDA = 0.165 * 1e-3 / scale  # uS
+    g_max_E2I_AMPA = 0.04 * 1e-3 / scale  # uS
+    g_max_E2I_NMDA = 0.13 * 1e-3 / scale  # uS
+    g_max_I2E_GABAa = 1.3 * 1e-3 / scale  # uS
+    g_max_I2I_GABAa = 1.0 * 1e-3 / scale  # uS
+
+    # E neurons/pyramid neurons
+    A = LIF(num_A, Cm=0.5, gL=0.025, t_refractory=2.)
+    B = LIF(num_B, Cm=0.5, gL=0.025, t_refractory=2.)
+    N = LIF(num_N, Cm=0.5, gL=0.025, t_refractory=2.)
+    # I neurons/interneurons
+    I = LIF(num_inh, Cm=0.2, gL=0.020, t_refractory=1.)
+
+    IA = PoissonStim(num_A, freq_var=10., t_interval=50.,
+                     freq_mean=mu0 + mu0 / 100. * coherence)
+    IB = PoissonStim(num_B, freq_var=10., t_interval=50.,
+                     freq_mean=mu0 - mu0 / 100. * coherence)
+
+    self.A = A
+    self.B = B
+    self.N = N
+    self.I = I
+    self.IA = IA
+    self.IB = IB
+    self.noise_A = PoissonNoise(num_A, freq=poisson_freq)
+    self.noise_B = PoissonNoise(num_B, freq=poisson_freq)
+    self.noise_N = PoissonNoise(num_N, freq=poisson_freq)
+    self.noise_I = PoissonNoise(num_inh, freq=poisson_freq)
+
+    # define external inputs
+    self.IA2A = AMPA_One(pre=IA, post=A, g_max=g_max_ext2E_AMPA)
+    self.IB2B = AMPA_One(pre=IB, post=B, g_max=g_max_ext2E_AMPA)
+
+    # define E2E conn
+    self.A2A_AMPA = AMPA(pre=A, post=A, g_max=g_max_E2E_AMPA * w_pos)
+    self.A2A_NMDA = NMDA(pre=A, post=A, g_max=g_max_E2E_NMDA * w_pos)
+
+    self.A2B_AMPA = AMPA(pre=A, post=B, g_max=g_max_E2E_AMPA * w_neg)
+    self.A2B_NMDA = NMDA(pre=A, post=B, g_max=g_max_E2E_NMDA * w_neg)
+
+    self.A2N_AMPA = AMPA(pre=A, post=N, g_max=g_max_E2E_AMPA)
+    self.A2N_NMDA = NMDA(pre=A, post=N, g_max=g_max_E2E_NMDA)
+
+    self.B2A_AMPA = AMPA(pre=B, post=A, g_max=g_max_E2E_AMPA * w_neg)
+    self.B2A_NMDA = NMDA(pre=B, post=A, g_max=g_max_E2E_NMDA * w_neg)
+
+    self.B2B_AMPA = AMPA(pre=B, post=B, g_max=g_max_E2E_AMPA * w_pos)
+    self.B2B_NMDA = NMDA(pre=B, post=B, g_max=g_max_E2E_NMDA * w_pos)
+
+    self.B2N_AMPA = AMPA(pre=B, post=N, g_max=g_max_E2E_AMPA)
+    self.B2N_NMDA = NMDA(pre=B, post=N, g_max=g_max_E2E_NMDA)
+
+    self.N2A_AMPA = AMPA(pre=N, post=A, g_max=g_max_E2E_AMPA * w_neg)
+    self.N2A_NMDA = NMDA(pre=N, post=A, g_max=g_max_E2E_NMDA * w_neg)
+
+    self.N2B_AMPA = AMPA(pre=N, post=B, g_max=g_max_E2E_AMPA * w_neg)
+    self.N2B_NMDA = NMDA(pre=N, post=B, g_max=g_max_E2E_NMDA * w_neg)
+
+    self.N2N_AMPA = AMPA(pre=N, post=N, g_max=g_max_E2E_AMPA)
+    self.N2N_NMDA = NMDA(pre=N, post=N, g_max=g_max_E2E_NMDA)
+
+    # define E2I conn
+    self.A2I_AMPA = AMPA(pre=A, post=I, g_max=g_max_E2I_AMPA)
+    self.A2I_NMDA = NMDA(pre=A, post=I, g_max=g_max_E2I_NMDA)
+
+    self.B2I_AMPA = AMPA(pre=B, post=I, g_max=g_max_E2I_AMPA)
+    self.B2I_NMDA = NMDA(pre=B, post=I, g_max=g_max_E2I_NMDA)
+
+    self.N2I_AMPA = AMPA(pre=N, post=I, g_max=g_max_E2I_AMPA)
+    self.N2I_NMDA = NMDA(pre=N, post=I, g_max=g_max_E2I_NMDA)
+
+    self.I2A_GABAa = GABAa(pre=I, post=A, g_max=g_max_I2E_GABAa)
+    self.I2B_GABAa = GABAa(pre=I, post=B, g_max=g_max_I2E_GABAa)
+    self.I2N_GABAa = GABAa(pre=I, post=N, g_max=g_max_I2E_GABAa)
+
+    # define I2I conn
+    self.I2I_GABAa = GABAa(pre=I, post=I, g_max=g_max_I2I_GABAa)
+
+    # define external projections
+    self.noise2A = AMPA_One(pre=self.noise_A, post=A, g_max=g_max_ext2E_AMPA)
+    self.noise2B = AMPA_One(pre=self.noise_B, post=B, g_max=g_max_ext2E_AMPA)
+    self.noise2N = AMPA_One(pre=self.noise_N, post=N, g_max=g_max_ext2E_AMPA)
+    self.noise2I = AMPA_One(pre=self.noise_I, post=I, g_max=g_max_ext2I_AMPA)
+
+
+# %%
+net = DecisionMaking(scale=1.)
+
+# %%
+runner = bp.StructRunner(net, monitors=['A.spike', 'B.spike', 'IA.freq', 'IB.freq'],
+                         dyn_vars=net.vars(), jit=True)
+pre_stimulus_period = 100.
+stimulus_period = 1000.
+delay_period = 500.
+total_period = pre_stimulus_period + stimulus_period + delay_period
+t = runner(total_period)
+print(f'Used time: {t} s')
+
+# %% [markdown]
+# ## Visualization
+
+# %%
+fig, gs = bp.visualize.get_figure(4, 1, 3, 10)
+
+runner.mon.numpy()
+t_start = 0.
+fig.add_subplot(gs[0, 0])
+bp.visualize.raster_plot(runner.mon.ts, runner.mon['A.spike'], markersize=1)
+plt.title("Spiking activity of group A")
+plt.ylabel("Neuron Index")
+plt.xlim(t_start, total_period + 1)
+plt.axvline(pre_stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period + delay_period, linestyle='dashed')
+
+fig.add_subplot(gs[1, 0])
+bp.visualize.raster_plot(runner.mon.ts, runner.mon['B.spike'], markersize=1)
+plt.title("Spiking activity of group B")
+plt.ylabel("Neuron Index")
+plt.xlim(t_start, total_period + 1)
+plt.axvline(pre_stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period + delay_period, linestyle='dashed')
+
+fig.add_subplot(gs[2, 0])
+rateA = bp.measure.firing_rate(runner.mon['A.spike'], width=10.)
+rateB = bp.measure.firing_rate(runner.mon['B.spike'], width=10.)
+plt.plot(runner.mon.ts, rateA, label="Group A")
+plt.plot(runner.mon.ts, rateB, label="Group B")
+plt.ylabel('Firing rate [Hz]')
+plt.title("Population activity")
+plt.xlim(t_start, total_period + 1)
+plt.axvline(pre_stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period + delay_period, linestyle='dashed')
+plt.legend()
+
+fig.add_subplot(gs[3, 0])
+plt.plot(runner.mon.ts, runner.mon['IA.freq'], label="group A")
+plt.plot(runner.mon.ts, runner.mon['IB.freq'], label="group B")
+plt.title("Input activity")
+plt.ylabel("Firing rate [Hz]")
+plt.xlim(t_start, total_period + 1)
+plt.axvline(pre_stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period, linestyle='dashed')
+plt.axvline(pre_stimulus_period + stimulus_period + delay_period, linestyle='dashed')
+plt.legend()
+
+plt.xlabel("Time [ms]")
+plt.show()
---
+++ examples/Yang_2020_RNN_Analysis.py
@@ -0,0 +1,317 @@
+# %% [markdown]
+# # *(Yang, 2020)*: Dynamical system analysis for RNN
+
+# %% [markdown]
+# Implementation of the paper:
+#
+# - Yang G R, Wang X J. Artificial neural networks for neuroscientists: A primer[J]. Neuron, 2020, 107(6): 1048-1070.
+#
+# The original implementation is based on PyTorch: https://github.com/gyyang/nn-brain/blob/master/RNN%2BDynamicalSystemAnalysis.ipynb
+
+# %%
+import brainpy as bp
+import brainpy.math as bm
+bp.math.set_platform('cpu')
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+# %% [markdown]
+# In this tutorial, we will use supervised learning to train a recurrent neural network on a simple perceptual decision making task, and analyze the trained network using dynamical system analysis.
+
+# %% [markdown]
+# ## Defining a cognitive task
+
+# %%
+# We will import the task from the neurogym library.
+# Please install neurogym:
+# 
+# https://github.com/neurogym/neurogym
+
+import neurogym as ngym
+
+# %%
+# Environment
+task = 'PerceptualDecisionMaking-v0'
+kwargs = {'dt': 100}
+seq_len = 100
+
+# Make supervised dataset
+dataset = ngym.Dataset(task, env_kwargs=kwargs, batch_size=16,
+                       seq_len=seq_len)
+
+# A sample environment from dataset
+env = dataset.env
+# Visualize the environment with 2 sample trials
+_ = ngym.utils.plot_env(env, num_trials=2, fig_kwargs={'figsize': (8, 6)})
+
+# %%
+input_size = env.observation_space.shape[0]
+output_size = env.action_space.n
+batch_size = dataset.batch_size
+
+
+# %% [markdown]
+# ## Define a vanilla continuous-time recurrent network
+
+# %% [markdown]
+# Here we will define a continuous-time neural network but discretize it in time using the Euler method.
+# \begin{align}
+#     \tau \frac{d\mathbf{r}}{dt} = -\mathbf{r}(t) + f(W_r \mathbf{r}(t) + W_x \mathbf{x}(t) + \mathbf{b}_r).
+# \end{align}
+#
+# This continuous-time system can then be discretized using the Euler method with a time step of $\Delta t$, 
+# \begin{align}
+#     \mathbf{r}(t+\Delta t) = \mathbf{r}(t) + \Delta \mathbf{r} = \mathbf{r}(t) + \frac{\Delta t}{\tau}[-\mathbf{r}(t) + f(W_r \mathbf{r}(t) + W_x \mathbf{x}(t) + \mathbf{b}_r)].
+# \end{align}
+
+# %%
+class RNN(bp.layers.Module):
+  def __init__(self, num_input, num_hidden, num_output, num_batch, dt=None, seed=None,
+               w_ir=bp.init.KaimingNormal(scale=1.),
+               w_rr=bp.init.KaimingNormal(scale=1.),
+               w_ro=bp.init.KaimingNormal(scale=1.)):
+    super(RNN, self).__init__()
+
+    # parameters
+    self.tau = 100
+    self.num_batch = num_batch
+    self.num_input = num_input
+    self.num_hidden = num_hidden
+    self.num_output = num_output
+    if dt is None:
+      self.alpha = 1
+    else:
+      self.alpha = dt / self.tau
+    self.rng = bm.random.RandomState(seed=seed)
+
+    # input weight
+    self.w_ir = self.get_param(w_ir, (num_input, num_hidden))
+
+    # recurrent weight
+    bound = 1 / num_hidden ** 0.5
+    self.w_rr = self.get_param(w_rr, (num_hidden, num_hidden))
+    self.b_rr = bm.TrainVar(self.rng.uniform(-bound, bound, num_hidden))
+
+    # readout weight
+    self.w_ro = self.get_param(w_ro, (num_hidden, num_output))
+    self.b_ro = bm.TrainVar(self.rng.uniform(-bound, bound, num_output))
+
+    # variables
+    self.h = bm.Variable(bm.zeros((num_batch, num_hidden)))
+    self.o = bm.Variable(bm.zeros((num_batch, num_output)))
+
+  def cell(self, x, h):
+    ins = x @ self.w_ir + h @ self.w_rr + self.b_rr
+    state = h * (1 - self.alpha) + ins * self.alpha
+    return bm.relu(state)
+
+  def readout(self, h):
+    return h @ self.w_ro + self.b_ro
+
+  def make_update(self, h: bm.JaxArray, o: bm.JaxArray):
+    def f(x):
+      h.value = self.cell(x, h.value)
+      o.value = self.readout(h.value)
+
+    return f
+
+  def predict(self, xs):
+    self.h[:] = 0.
+    f = bm.make_loop(self.make_update(self.h, self.o),
+                     dyn_vars=self.vars(),
+                     out_vars=[self.h, self.o])
+    return f(xs)
+
+  def loss(self, xs, ys):
+    hs, os = self.predict(xs)
+    os = os.reshape((-1, os.shape[-1]))
+    loss = bm.losses.cross_entropy_loss(os, ys.flatten())
+    return loss, os
+
+
+# %% [markdown]
+# ## Train the recurrent network on the decision-making task
+
+# %%
+# Instantiate the network and print information
+hidden_size = 64
+net = RNN(num_input=input_size,
+          num_hidden=hidden_size,
+          num_output=output_size,
+          num_batch=batch_size,
+          dt=env.dt)
+
+# %%
+# prediction method
+predict = bm.jit(net.predict, dyn_vars=net.vars())
+
+# Adam optimizer
+opt = bm.optimizers.Adam(lr=0.001, train_vars=net.train_vars().unique())
+
+# gradient function
+grad_f = bm.grad(net.loss,
+                 dyn_vars=net.vars(),
+                 grad_vars=net.train_vars().unique(),
+                 return_value=True,
+                 has_aux=True)
+
+# training function
+@bm.jit
+@bm.function(nodes=(net, opt))
+def train(xs, ys):
+  grads, (loss, os) = grad_f(xs, ys)
+  opt.update(grads)
+  return loss, os
+
+
+# %%
+running_acc = 0
+running_loss = 0
+for i in range(1500):
+  inputs, labels_np = dataset()
+  inputs = bm.asarray(inputs)
+  labels = bm.asarray(labels_np)
+  loss, outputs = train(inputs, labels)
+  running_loss += loss
+  # Compute performance
+  output_np = np.argmax(outputs.numpy(), axis=-1).flatten()
+  labels_np = labels_np.flatten()
+  ind = labels_np > 0  # Only analyze time points when target is not fixation
+  running_acc += np.mean(labels_np[ind] == output_np[ind])
+  if i % 100 == 99:
+    running_loss /= 100
+    running_acc /= 100
+    print('Step {}, Loss {:0.4f}, Acc {:0.3f}'.format(i + 1, running_loss, running_acc))
+    running_loss = 0
+    running_acc = 0
+
+# %% [markdown]
+# ## Visualize neural activity for in sample trials
+#
+# We will run the network for 100 sample trials, then visual the neural activity trajectories in a PCA space.
+
+# %%
+env.reset(no_step=True)
+perf = 0
+num_trial = 100
+activity_dict = {}
+trial_infos = {}
+for i in range(num_trial):
+    env.new_trial()
+    ob, gt = env.ob, env.gt
+    inputs = bm.asarray(ob[:, np.newaxis, :])
+    rnn_activity, action_pred = predict(inputs)
+    rnn_activity = rnn_activity.numpy()[:, 0, :]
+    activity_dict[i] = rnn_activity
+    trial_infos[i] = env.trial
+    
+# Concatenate activity for PCA
+activity = np.concatenate(list(activity_dict[i] for i in range(num_trial)), axis=0)
+print('Shape of the neural activity: (Time points, Neurons): ', activity.shape)
+
+# Print trial informations
+for i in range(5):
+    print('Trial ', i, trial_infos[i])
+
+# %%
+pca = PCA(n_components=2)
+pca.fit(activity)
+
+# %% [markdown]
+# Transform individual trials and Visualize in PC space based on ground-truth color. We see that the neural activity is organized by stimulus ground-truth in PC1
+
+# %%
+plt.rcdefaults()
+fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, sharex=True, figsize=(12, 5))
+for i in range(num_trial):
+    activity_pc = pca.transform(activity_dict[i])
+    trial = trial_infos[i]
+    color = 'red' if trial['ground_truth'] == 0 else 'blue'
+    _ = ax1.plot(activity_pc[:, 0], activity_pc[:, 1], 'o-', color=color)
+    if i < 5:
+        _ = ax2.plot(activity_pc[:, 0], activity_pc[:, 1], 'o-', color=color)
+
+ax1.set_xlabel('PC 1')
+ax1.set_ylabel('PC 2')
+plt.show()
+
+# %% [markdown]
+# ## Search for approximate fixed points
+
+# %% [markdown]
+# Here we search for approximate fixed points and visualize them in the same PC space. In a generic dynamical system,
+# \begin{align}
+#     \frac{d\mathbf{x}}{dt} = F(\mathbf{x}),
+# \end{align}
+# We can search for fixed points by doing the optimization
+# \begin{align}
+#     \mathrm{argmin}_{\mathbf{x}} |F(\mathbf{x})|^2.
+# \end{align}
+
+# %%
+f_cell = lambda h: net.cell(bm.asarray([1, 0.5, 0.5]), h)
+
+# %%
+fp_candidates = bm.vstack([activity_dict[i] for i in range(num_trial)])
+fp_candidates.shape
+
+# %%
+finder = bp.numeric.FixedPointFinder(
+  f_cell=f_cell,
+  f_type='F',
+  tol_opt=1e-5,
+  tol_speed=1e-5,
+  tol_unique=0.03,
+  tol_outlier=0.1,
+  opt_setting=dict(method=bm.optimizers.Adam,
+                   lr=bm.optimizers.ExponentialDecay(0.01, 1, 0.9999)),
+  num_opt_batch=200, 
+)
+fixed_points, _, _, _ = finder.find_fixed_points(candidates=fp_candidates)
+
+# %% [markdown]
+# ## Visualize the found approximate fixed points.
+#
+# We see that they found an approximate line attrator, corresponding to our PC1, along which evidence is integrated during the stimulus period.
+
+# %%
+# Plot in the same space as activity
+plt.figure(figsize=(10, 5))
+for i in range(10):
+    activity_pc = pca.transform(activity_dict[i])
+    trial = trial_infos[i]
+    color = 'red' if trial['ground_truth'] == 0 else 'blue'
+    plt.plot(activity_pc[:, 0], activity_pc[:, 1], 'o-', color=color, alpha=0.1)
+
+# Fixed points are shown in cross
+fixedpoints_pc = pca.transform(fixed_points)
+plt.plot(fixedpoints_pc[:, 0], fixedpoints_pc[:, 1], 'x', label='fixed points')
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend()
+plt.show()
+
+# %% [markdown]
+# ## Computing the Jacobian and finding the line attractor
+
+# %%
+from jax import jacobian
+
+# %%
+dFdh = jacobian(f_cell)(fixed_points[10])
+
+eigval, eigvec = np.linalg.eig(dFdh.numpy())
+
+# %%
+# Plot distribution of eigenvalues in a 2-d real-imaginary plot
+plt.figure()
+plt.scatter(np.real(eigval), np.imag(eigval))
+plt.plot([1, 1], [-1, 1], '--')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
