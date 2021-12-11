357a371f4b182e3a2b7b59479d97279586f236e5
chaoming <chao.brain@qq.com>
2021-12-11 14:31:22 +0800

    add `event_add_v2` cpu operators and `event_add` and `event_add_v2` gpu operators

--- brainpy_extensions/brainpylib/_event_add.py
+++ brainpy_extensions/brainpylib/_event_add.py
@@ -23,15 +23,27 @@ for _name, _value in cpu_ops.registrations().items():
 
 def event_add(events, pre2post, post_num, value):
   # output value
-  value = jnp.array(value)
+  value = jnp.asarray(value)
   out = jnp.zeros(post_num, dtype=value.dtype)
   # pre2post connection
-  post_ids, pre2post_slice = pre2post
-  assert len(events) + 1 == len(pre2post_slice)
-  return _event_add_prim.bind(events, post_ids, pre2post_slice, value, out)
+  indices, indptr = pre2post
+  assert len(events) + 1 == len(indptr)
+  return _event_add_prim.bind(events, indices, indptr, value, out)
 
 
 _event_add_prim = core.Primitive("event_add")
+
+
+def event_add_v2(events, pre_ids, post_ids, post_num, value):
+  # output value
+  value = jnp.asarray(value)
+  out = jnp.zeros(post_num, dtype=value.dtype)
+  # connections
+  assert len(pre_ids) == len(post_ids)
+  return _event_add_v2_prim.bind(events, pre_ids, post_ids, value, out)
+
+
+_event_add_v2_prim = core.Primitive("event_add_v2")
 
 
 # *********************************
@@ -40,9 +52,9 @@ _event_add_prim = core.Primitive("event_add")
 
 # For JIT compilation we need a function to evaluate the shape and dtype of the
 # outputs of our op for some given inputs
-def _event_add_abstract(events, post_ids, pre2post_slice, value, out):
-  assert dtypes.canonicalize_dtype(post_ids.dtype) in [np.uint32, np.uint64]
-  assert dtypes.canonicalize_dtype(pre2post_slice.dtype) in [np.uint32, np.uint64]
+def _event_add_abstract(events, indices, indptr, value, out):
+  assert dtypes.canonicalize_dtype(indices.dtype) in [np.uint32, np.uint64]
+  assert dtypes.canonicalize_dtype(indptr.dtype) in [np.uint32, np.uint64]
   assert dtypes.canonicalize_dtype(events.dtype) == np.bool_
   assert dtypes.canonicalize_dtype(value.dtype) == dtypes.canonicalize_dtype(out.dtype)
   return out
@@ -52,10 +64,22 @@ _event_add_prim.def_abstract_eval(_event_add_abstract)
 _event_add_prim.def_impl(partial(xla.apply_primitive, _event_add_prim))
 
 
+def _event_add_v2_abstract(events, pre_ids, post_ids, value, out):
+  assert dtypes.canonicalize_dtype(pre_ids.dtype) in [np.uint32, np.uint64]
+  assert dtypes.canonicalize_dtype(post_ids.dtype) in [np.uint32, np.uint64]
+  assert dtypes.canonicalize_dtype(events.dtype) == np.bool_
+  assert dtypes.canonicalize_dtype(value.dtype) == dtypes.canonicalize_dtype(out.dtype)
+  return out
+
+_event_add_v2_prim.def_abstract_eval(_event_add_v2_abstract)
+_event_add_v2_prim.def_impl(partial(xla.apply_primitive, _event_add_v2_prim))
+
+
+
 # We also need a translation rule to convert the function into an XLA op. In
 # our case this is the custom XLA op that we've written. We're wrapping two
 # translation rules into one here: one for the CPU and one for the GPU
-def _event_slice_add_translation(c, events, post_ids, pre2post_slice, value, out, *, platform="cpu"):
+def _event_add_translation(c, events, indices, indptr, value, out, *, platform="cpu"):
   # The event shape
   events_shape = c.get_shape(events)
   events_dim = events_shape.dimensions()
@@ -67,12 +91,12 @@ def _event_slice_add_translation(c, events, post_ids, pre2post_slice, value, out
   _pre_shape = xla_client.Shape.array_shape(np.dtype(np.uint32), (), ())
 
   # The post_ids shape
-  post_ids_shape = c.get_shape(post_ids)
+  post_ids_shape = c.get_shape(indices)
   _post_ids_shape = xla_client.Shape.array_shape(
     post_ids_shape.element_type(), post_ids_shape.dimensions(), (0,))
 
   # The pre_slice shape
-  pre_slice_shape = c.get_shape(pre2post_slice)
+  pre_slice_shape = c.get_shape(indptr)
   _pre_slice_shape = xla_client.Shape.array_shape(
     pre_slice_shape.element_type(), pre_slice_shape.dimensions(), (0,))
 
@@ -102,8 +126,8 @@ def _event_slice_add_translation(c, events, post_ids, pre2post_slice, value, out
       op_name,  # call_target_name
       operands=(xla_client.ops.ConstantLiteral(c, pre_size),
                 events,
-                post_ids,
-                pre2post_slice,
+                indices,
+                indptr,
                 value),  # The inputs
       operand_shapes_with_layout=(_pre_shape,
                                   _events_shape,
@@ -112,9 +136,12 @@ def _event_slice_add_translation(c, events, post_ids, pre2post_slice, value, out
                                   _value_shape),  # The input shapes
       shape_with_layout=_out_shape,  # The output shapes
     )
+  elif platform == 'gpu':
+    pass
+
   raise ValueError("Unsupported platform; this must be either 'cpu' or 'gpu'")
 
 
 # Connect the XLA translation rules for JIT compilation
 xla.backend_specific_translations["cpu"][_event_add_prim] = \
-  partial(_event_slice_add_translation, platform="cpu")
+  partial(_event_add_translation, platform="cpu")
