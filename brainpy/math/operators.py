f5e156de950cc2a9e09ff93c10aab3e9b27219fb
chaoming <chao.brain@qq.com>
2022-01-24 17:37:44 +0800

    add "event_sum3" operator

--- brainpy/math/operators.py
+++ brainpy/math/operators.py
@@ -17,6 +17,7 @@ except ModuleNotFoundError:
 __all__ = [
   # pre-to-post
   'pre2post_event_sum',
+  'pre2post_event_sum2',
   'pre2post_sum',
   'pre2post_prod',
   'pre2post_max',
@@ -53,7 +54,7 @@ def _check_brainpylib(ops_name):
   )
 
 
-def pre2post_event_sum(events, pre2post, post_num, values):
+def pre2post_event_sum(events, pre2post, post_num, values=1.):
   """The pre-to-post synaptic computation with event-driven summation.
 
   When ``values`` is a scalar, this function is equivalent to
@@ -106,6 +107,70 @@ def pre2post_event_sum(events, pre2post, post_num, values):
   idnptr = as_device_array(idnptr)
   values = as_device_array(values)
   return brainpylib.event_sum(events, (indices, idnptr), post_num, values)
+
+
+def pre2post_event_sum2(events, pre_ids, post_ids, post_num, values=1.):
+  """The pre-to-post synaptic computation with event-driven summation.
+
+  When ``values`` is a scalar, this function is equivalent to
+
+  .. highlight:: python
+  .. code-block:: python
+
+    post_val = np.zeros(post_num)
+    post_ids, idnptr = pre2post
+    for i in range(pre_num):
+      if events[i]:
+        for j in range(idnptr[i], idnptr[i+1]):
+          post_val[post_ids[i]] += values
+
+  When ``values`` is a vector (with the length of ``len(post_ids)``),
+  this function is equivalent to
+
+  .. highlight:: python
+  .. code-block:: python
+
+    post_val = np.zeros(post_num)
+
+    post_ids, idnptr = pre2post
+    for i in range(pre_num):
+      if events[i]:
+        for j in range(idnptr[i], idnptr[i+1]):
+          post_val[post_ids[i]] += values[j]
+
+
+  Parameters
+  ----------
+  events: JaxArray, jax.numpy.ndarray, Variable
+    The events, must be bool.
+  pre2post: tuple of JaxArray, tuple of jax.numpy.ndarray
+    A tuple contains the connection information of pre-to-post.
+  post_num: int
+    The number of post-synaptic group.
+  values: float, JaxArray, jax.numpy.ndarray
+    The value to make summation.
+
+  Returns
+  -------
+  out: JaxArray, jax.numpy.ndarray
+    A tensor with the shape of ``post_num``.
+  """
+  _check_brainpylib(pre2post_event_sum2.__name__)
+  events = as_device_array(events)
+  pre_ids = as_device_array(pre_ids)
+  post_ids = as_device_array(post_ids)
+  values = as_device_array(values)
+  return brainpylib.event_sum2(events, pre_ids, post_ids, post_num, values)
+
+
+def pre2post_event_sum3(events, pre2post, post_num, values=1., max_post_conn=None):
+  _check_brainpylib(pre2post_event_sum3.__name__)
+  indices, idnptr = pre2post
+  events = as_device_array(events)
+  indices = as_device_array(indices)
+  idnptr = as_device_array(idnptr)
+  values = as_device_array(values)
+  return brainpylib.event_sum3(events, (indices, idnptr), post_num, values, max_post_conn)
 
 
 # def pre2post_event_sum2(events, pre_ids, post_ids, post_num, values):
