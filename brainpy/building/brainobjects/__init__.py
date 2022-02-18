2a763404a26f1f8a790ba250d8fc6f5c73ef273d
chaoming <chao.brain@qq.com>
2022-02-18 19:56:16 +0800

    move neurons and synapse modules into brainpy.brainobjects

--- brainpy/building/brainobjects/__init__.py
+++ brainpy/building/brainobjects/__init__.py
@@ -5,28 +5,6 @@ This module provides various interface to model brain objects.
 You can access them through ``brainpy.XXX`` or ``brainpy.brainobjects.XXX``.
 """
 
-
-__all__ = [
-  # area.py
-  'BrainArea',
-  # base.py
-  'DynamicalSystem', 'Container',
-  # delays.py
-  'Delay', 'ConstantDelay',
-  # molecular.py
-  'Molecular',
-  # network.py
-  'Network',
-  # neuron.py
-  'NeuGroup', 'Channel', 'Soma', 'Dendrite',
-  # synapse.py
-  'TwoEndConn',
-]
-
-from .area import *
 from .base import *
-from .delays import *
-from .molecular import *
-from .network import *
-from .neuron import *
-from .synapse import *
+from . import neurons
+from . import synapses
