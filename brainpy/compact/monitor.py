6b607e1c7e49c775cb914617d0c4911699b3dfdc
chaoming <chao.brain@qq.com>
2022-02-19 11:45:12 +0800

    updates

--- brainpy/compact/monitor.py
+++ brainpy/compact/monitor.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
+import warnings
 
-from brainpy.sim.monitor import Monitor
+from brainpy.sim import monitor
 
 __all__ = [
   'Monitor'
 ]
+
+
+class Monitor(monitor.Monitor):
+  def __init__(self, *args, **kwargs):
+    super(Monitor, self).__init__(*args, **kwargs)
+    warnings.warn('Please use "brainpy.sim.Monitor" instead. '
+                  '"brainpy.Monitor" will be removed since version 2.1.0.',
+                  DeprecationWarning)
--- brainpy/compact/runners.py
+++ brainpy/compact/runners.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from brainpy.sim.runners import IntegratorRunner, DSRunner, StructRunner, ReportRunner
+import warnings
+
+from brainpy.sim import runners
 
 __all__ = [
   'IntegratorRunner',
@@ -9,3 +11,22 @@ __all__ = [
   'ReportRunner'
 ]
 
+
+class IntegratorRunner(runners.IntegratorRunner):
+  def __init__(self, *args, **kwargs):
+    super(IntegratorRunner, self).__init__(*args, **kwargs)
+    warnings.warn('Please use "brainpy.sim.IntegratorRunner" instead. '
+                  '"brainpy.IntegratorRunner" will be removed since version 2.1.0.',
+                  DeprecationWarning)
+
+
+class DSRunner(runners.DSRunner):
+  def __init__(self, *args, **kwargs):
+    super(DSRunner, self).__init__(*args, **kwargs)
+    warnings.warn('Please use "brainpy.sim.DSRunner" instead. '
+                  '"brainpy.DSRunner" will be removed since version 2.1.0.',
+                  DeprecationWarning)
+
+
+StructRunner = runners.StructRunner
+ReportRunner = runners.ReportRunner
