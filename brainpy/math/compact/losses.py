6f07f91212770bb39808caf8b4c0200c3b702163
chaoming <chao.brain@qq.com>
2022-02-22 13:43:16 +0800

    updates

--- brainpy/math/compact/losses.py
+++ brainpy/math/compact/losses.py
@@ -18,55 +18,55 @@ __all__ = [
 
 def cross_entropy_loss(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.cross_entropy_loss(*args, **kwargs)
 
 
 def l1_loos(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.l1_loos(*args, **kwargs)
 
 
 def l2_loss(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.l2_loss(*args, **kwargs)
 
 
 def l2_norm(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.l2_norm(*args, **kwargs)
 
 
 def huber_loss(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.huber_loss(*args, **kwargs)
 
 
 def mean_absolute_error(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.mean_absolute_error(*args, **kwargs)
 
 
 def mean_squared_error(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.mean_squared_error(*args, **kwargs)
 
 
 def mean_squared_log_error(*args, **kwargs):
   warnings.warn('Please use "brainpy.losses.XXX" instead. '
-                '"brainpy.math.losses.XXX" will be removed since version 2.1.0. ',
+                '"brainpy.math.losses.XXX" is deprecated since version 2.0.3. ',
                 DeprecationWarning)
   return losses.mean_squared_log_error(*args, **kwargs)
