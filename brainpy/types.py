6952bc9ef8e645f1d1588498b37ed18b939fbc37
chaoming <adaduo@outlook.com>
2022-03-06 15:01:16 +0800

    add "Parameter" and "Shape" types

--- brainpy/types.py
+++ brainpy/types.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from typing import TypeVar
+from typing import TypeVar, Tuple
 
 import jax.numpy as jnp
 
@@ -8,6 +8,10 @@ import brainpy.math as bm
 
 __all__ = [
   'Tensor',
+  'Parameter',
+  'Shape',
 ]
 
 Tensor = TypeVar('Tensor', bm.JaxArray, jnp.ndarray)
+Parameter = TypeVar('Parameter', bm.JaxArray, jnp.ndarray, float, int, bm.Variable)
+Shape = TypeVar('Shape', int, Tuple[int, ...])
