f7218a56aceb605d509b2b2b21bce254fccab09c
Brandon Zhang <ztqakita@163.com>
2022-04-10 22:22:37 +0800

    Update test_conv.py

--- brainpy/nn/nodes/ANN/tests/test_conv.py
+++ brainpy/nn/nodes/ANN/tests/test_conv.py
@@ -9,24 +9,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class TestConv(TestCase):
-  def test_Conv2D(self):
+  def test_Conv2D_img(self):
     i = bp.nn.Input((200, 198, 3))
     b = bp.nn.Conv2D(3, 32, (3, 3))
     model = i >> b
     model.initialize(num_batch=2)
 
-    img = jnp.zeros((2, 200, 198, 3), dtype=jnp.float32)
+    img = jnp.zeros((2, 200, 198, 3))
     for k in range(3):
       x = 30 + 60 * k
       y = 20 + 60 * k
       img = img.at[0, x:x + 10, y:y + 10, k].set(1.0)
       img = img.at[1, x:x + 20, y:y + 20, k].set(3.0)
 
-    # img = jnp.transpose(img, [0, 3, 1, 2])
-
     out = model(img)
     print("out shape: ", out.shape)
-    print(out[0])
     # print("First output channel:")
     # plt.figure(figsize=(10, 10))
     # plt.imshow(np.array(out)[0, :, :, 0])
