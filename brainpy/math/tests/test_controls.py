1689c4979ec0c2726967078d6a1ca6964454dd28
chaoming <adaduo@outlook.com>
2022-04-24 10:39:17 +0800

    fix #120: JaxArray raises errors when updated in a JIT function

--- brainpy/math/tests/test_controls.py
+++ brainpy/math/tests/test_controls.py
@@ -1,50 +1,69 @@
 # -*- coding: utf-8 -*-
 
-
+import unittest
+import brainpy as bp
 import brainpy.math as bm
 
 
-def test_easy_scan1():
+class TestScan(unittest.TestCase):
+  def test_easy_scan1(self):
+    def make_node(v1, v2):
+      def update(x):
+        v1.value = v1 * x
+        return (v1 + v2) * x
 
-  def make_node(v1, v2):
-    def update(x):
-      v1.value = v1 * x
-      return (v1 + v2) * x
+      return update
 
-    return update
+    _v1 = bm.Variable(bm.random.normal(size=10))
+    _v2 = bm.Variable(bm.random.random(size=10))
+    _xs = bm.random.uniform(size=(4, 10))
 
-  _v1 = bm.random.normal(size=10)
-  _v2 = bm.random.random(size=10)
-  _xs = bm.random.uniform(size=(4, 10))
+    scan_f = bm.make_loop(make_node(_v1, _v2),
+                          dyn_vars=(_v1, _v2),
+                          out_vars=(_v1,),
+                          has_return=True)
+    outs, returns = scan_f(_xs)
+    for out in outs:
+      print(out.shape)
+    print(outs)
+    print(returns.shape)
+    print(returns)
 
-  scan_f = bm.make_loop(make_node(_v1, _v2),
-                        dyn_vars=(_v1, _v2),
-                        out_vars=(_v1,),
-                        has_return=True)
-  outs, returns = scan_f(_xs)
-  for out in outs:
-    print(out.shape)
-  print(outs)
-  print(returns.shape)
-  print(returns)
+    print('-' * 20)
+    scan_f = bm.make_loop(make_node(_v1, _v2),
+                          dyn_vars=(_v1, _v2),
+                          out_vars=_v1,
+                          has_return=True)
+    outs, returns = scan_f(_xs)
+    print(outs.shape)
+    print(outs)
+    print(returns.shape)
+    print(returns)
 
-  print('-' * 20)
-  scan_f = bm.make_loop(make_node(_v1, _v2),
-                        dyn_vars=(_v1, _v2),
-                        out_vars=_v1,
-                        has_return=True)
-  outs, returns = scan_f(_xs)
-  print(outs.shape)
-  print(outs)
-  print(returns.shape)
-  print(returns)
+    print('-' * 20)
+    scan_f = bm.make_loop(make_node(_v1, _v2),
+                          dyn_vars=(_v1, _v2),
+                          has_return=True)
+    outs, returns = scan_f(_xs)
+    print(outs)
+    print(returns.shape)
+    print(returns)
 
-  print('-' * 20)
-  scan_f = bm.make_loop(make_node(_v1, _v2),
-                        dyn_vars=(_v1, _v2),
-                        has_return=True)
-  outs, returns = scan_f(_xs)
-  print(outs)
-  print(returns.shape)
-  print(returns)
+  def test_jaxarray(self):
+    def make_node(v1, v2):
+      def update(x):
+        v1.value = v1 * x
+        return (v1 + v2) * x
 
+      return update
+
+    _v1 = bm.random.normal(size=10)
+    _v2 = bm.random.random(size=10)
+    _xs = bm.random.uniform(size=(4, 10))
+
+    scan_f = bm.make_loop(make_node(_v1, _v2),
+                          dyn_vars=(_v1, _v2),
+                          out_vars=(_v1,),
+                          has_return=True)
+    with self.assertRaises(bp.errors.MathError):
+      outs, returns = scan_f(_xs)
