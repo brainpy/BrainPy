75b992f30bc0a852c64f576691c56cfd379bf9ed
Xiaoyu Chen <55552143+c-xy17@users.noreply.github.com>
2022-05-11 16:11:30 +0800

    add @wraps(np.random.xxx) for API documentation

--- brainpy/math/random.py
+++ brainpy/math/random.py
@@ -7,6 +7,8 @@ from jax.tree_util import register_pytree_node
 
 from brainpy.math.jaxarray import JaxArray, Variable
 
+from .utils import wraps
+
 
 __all__ = [
   'RandomState',
@@ -207,28 +209,34 @@ register_pytree_node(RandomState,
 DEFAULT = RandomState(np.random.randint(0, 10000, size=2, dtype=np.uint32))
 
 
+@wraps(np.random.seed)
 def seed(seed=None):
   global DEFAULT
   DEFAULT.seed(np.random.randint(0, 100000) if seed is None else seed)
 
 
+@wraps(np.random.rand)
 def rand(*dn):
   return JaxArray(jr.uniform(DEFAULT.split_key(), shape=dn, minval=0., maxval=1.))
 
 
+@wraps(np.random.randint)
 def randint(low, high=None, size=None, dtype=int):
   return JaxArray(jr.randint(DEFAULT.split_key(), shape=_size2shape(size),
                              minval=low, maxval=high, dtype=dtype))
 
 
+@wraps(np.random.randn)
 def randn(*dn):
   return JaxArray(jr.normal(DEFAULT.split_key(), shape=dn))
 
 
+@wraps(np.random.random)
 def random(size=None):
   return JaxArray(jr.uniform(DEFAULT.split_key(), shape=_size2shape(size), minval=0., maxval=1.))
 
 
+@wraps(np.random.random_sample)
 def random_sample(size=None):
   return JaxArray(jr.uniform(DEFAULT.split_key(), shape=_size2shape(size), minval=0., maxval=1.))
 
@@ -237,87 +245,105 @@ ranf = random_sample
 sample = random_sample
 
 
+@wraps(np.random.choice)
 def choice(a, size=None, replace=True, p=None):
   a = a.value if isinstance(a, JaxArray) else a
   return JaxArray(jr.choice(DEFAULT.split_key(), a=a, shape=_size2shape(size), replace=replace, p=p))
 
 
+@wraps(np.random.permutation)
 def permutation(x):
   x = x.value if isinstance(x, JaxArray) else x
   return JaxArray(jr.permutation(DEFAULT.split_key(), x))
 
 
+@wraps(np.random.shuffle)
 def shuffle(x, axis=0):
   assert isinstance(x, JaxArray), f'Must be a JaxArray, but got {type(x)}'
   x.value = jr.permutation(DEFAULT.split_key(), x.value, axis=axis)
 
 
+@wraps(np.random.beta)
 def beta(a, b, size=None):
   a = a.value if isinstance(a, JaxArray) else a
   b = b.value if isinstance(b, JaxArray) else b
   return JaxArray(jr.beta(DEFAULT.split_key(), a=a, b=b, shape=_size2shape(size)))
 
 
+@wraps(np.random.exponential)
 def exponential(scale=1.0, size=None):
   assert scale == 1.
   return JaxArray(jr.exponential(DEFAULT.split_key(), shape=_size2shape(size)))
 
 
+@wraps(np.random.gamma)
 def gamma(shape, scale=1.0, size=None):
   assert scale == 1.
   return JaxArray(jr.gamma(DEFAULT.split_key(), a=shape, shape=_size2shape(size)))
 
 
+@wraps(np.random.gumbel)
 def gumbel(loc=0.0, scale=1.0, size=None):
   assert loc == 0.
   assert scale == 1.
   return JaxArray(jr.gumbel(DEFAULT.split_key(), shape=_size2shape(size)))
 
 
+@wraps(np.random.laplace)
 def laplace(loc=0.0, scale=1.0, size=None):
   assert loc == 0.
   assert scale == 1.
   return JaxArray(jr.laplace(DEFAULT.split_key(), shape=_size2shape(size)))
 
 
+@wraps(np.random.logistic)
 def logistic(loc=0.0, scale=1.0, size=None):
   assert loc == 0.
   assert scale == 1.
   return JaxArray(jr.logistic(DEFAULT.split_key(), shape=_size2shape(size)))
 
 
+@wraps(np.random.normal)
 def normal(loc=0.0, scale=1.0, size=None):
   return JaxArray(jr.normal(DEFAULT.split_key(), shape=_size2shape(size)) * scale + loc)
 
 
+@wraps(np.random.pareto)
 def pareto(a, size=None):
   return JaxArray(jr.pareto(DEFAULT.split_key(), b=a, shape=_size2shape(size)))
 
 
+@wraps(np.random.poisson)
 def poisson(lam=1.0, size=None):
   return JaxArray(jr.poisson(DEFAULT.split_key(), lam=lam, shape=_size2shape(size)))
 
 
+@wraps(np.random.standard_cauchy)
 def standard_cauchy(size=None):
   return JaxArray(jr.cauchy(DEFAULT.split_key(), shape=_size2shape(size)))
 
 
+@wraps(np.random.standard_exponential)
 def standard_exponential(size=None):
   return JaxArray(jr.exponential(DEFAULT.split_key(), shape=_size2shape(size)))
 
 
+@wraps(np.random.standard_gamma)
 def standard_gamma(shape, size=None):
   return JaxArray(jr.gamma(DEFAULT.split_key(), a=shape, shape=_size2shape(size)))
 
 
+@wraps(np.random.standard_normal)
 def standard_normal(size=None):
   return JaxArray(jr.normal(DEFAULT.split_key(), shape=_size2shape(size)))
 
 
+@wraps(np.random.standard_t)
 def standard_t(df, size=None):
   return JaxArray(jr.t(DEFAULT.split_key(), df=df, shape=_size2shape(size)))
 
 
+@wraps(np.random.uniform)
 def uniform(low=0.0, high=1.0, size=None):
   return JaxArray(jr.uniform(DEFAULT.split_key(), shape=_size2shape(size), minval=low, maxval=high))
 
@@ -373,6 +399,7 @@ def bernoulli(p, size=None):
   return JaxArray(jr.bernoulli(DEFAULT.split_key(), p=p, shape=_size2shape(size)))
 
 
+@wraps(np.random.lognormal)
 def lognormal(mean=0.0, sigma=1.0, size=None):
   samples = jr.normal(DEFAULT.split_key(), shape=_size2shape(size))
   samples = samples * sigma + mean
