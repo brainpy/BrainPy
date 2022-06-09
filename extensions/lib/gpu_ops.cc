9f1ea2810f3a1b70800646179492199342c6c9f3
chaoming <adaduo@outlook.com>
2022-06-09 12:06:23 +0800

    update brainpylib

--- extensions/lib/gpu_ops.cc
+++ extensions/lib/gpu_ops.cc
@@ -26,6 +26,17 @@ namespace {
         dict["gpu_event_sum_heter_f64_i32"] = EncapsulateFunction(gpu_event_sum_heter_f64_i32);
         dict["gpu_event_sum_heter_f64_i64"] = EncapsulateFunction(gpu_event_sum_heter_f64_i64);
 
+        // homogeneous event_sum2
+        dict["gpu_event_sum2_homo_f32_i32"] = EncapsulateFunction(gpu_event_sum2_homo_f32_i32);
+        dict["gpu_event_sum2_homo_f32_i64"] = EncapsulateFunction(gpu_event_sum2_homo_f32_i64);
+        dict["gpu_event_sum2_homo_f64_i32"] = EncapsulateFunction(gpu_event_sum2_homo_f64_i32);
+        dict["gpu_event_sum2_homo_f64_i64"] = EncapsulateFunction(gpu_event_sum2_homo_f64_i64);
+        // heterogeneous event_sum2
+        dict["gpu_event_sum2_heter_f32_i32"] = EncapsulateFunction(gpu_event_sum2_heter_f32_i32);
+        dict["gpu_event_sum2_heter_f32_i64"] = EncapsulateFunction(gpu_event_sum2_heter_f32_i64);
+        dict["gpu_event_sum2_heter_f64_i32"] = EncapsulateFunction(gpu_event_sum2_heter_f64_i32);
+        dict["gpu_event_sum2_heter_f64_i64"] = EncapsulateFunction(gpu_event_sum2_heter_f64_i64);
+
         // homogeneous atomic_sum
         dict["gpu_atomic_sum_homo_f32_i32"] = EncapsulateFunction(gpu_atomic_sum_homo_f32_i32);
         dict["gpu_atomic_sum_homo_f32_i64"] = EncapsulateFunction(gpu_atomic_sum_homo_f32_i64);
@@ -55,6 +66,7 @@ namespace {
     ) {
     m.def("registrations", &Registrations);
     m.def("build_event_sum_descriptor", &build_event_sum_descriptor);
+    m.def("build_event_sum2_descriptor", &build_event_sum2_descriptor);
     m.def("build_atomic_sum_descriptor", &build_atomic_sum_descriptor);
     m.def("build_atomic_prod_descriptor", &build_atomic_prod_descriptor);
 }
