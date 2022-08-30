492332c0ea27bf40e3e17a06657c4922464d9cc5
chaoming <adaduo@outlook.com>
2022-08-30 17:06:08 +0800

    add DotDict doc

--- brainpy/tools/others/dicts.py
+++ brainpy/tools/others/dicts.py
@@ -24,10 +24,34 @@ class DotDict(dict):
   >>> d.c = 30  # but you can assign a value to a non-existing item
   >>> d.c
   30
+
+  In general, all attributes will be included ad keys in the dict.
+  For example, if you add an attribute to specify what variable names
+  you have:
+
+  >>> d.names = ('a', 'b')
+
+  This attribute `names` will cause error when you treat the object as
+  a PyTree.
+
+  >>> from jax import jit
+  >>> f = jit(lambda x: x)
+  >>> f(d)
+  TypeError: Argument 'a' of type <class 'str'> is not a valid JAX type.
+
+  At this moment, you can label this attribute `names` as not a key in the dictionary
+  by using the syntax::
+
+  >>> d.add_attr_not_key('names')
+  >>> f(d)
+  {'a': DeviceArray(10, dtype=int32, weak_type=True),
+   'b': DeviceArray(20, dtype=int32, weak_type=True),
+   'c': DeviceArray(30, dtype=int32, weak_type=True)}
+
   """
 
   '''Used to exclude variables that '''
-  excluded_vars = ('excluded_vars', 'var_names')
+  attrs_not_keys = ('attrs_not_keys', 'var_names')
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
@@ -38,7 +62,7 @@ class DotDict(dict):
     """Retrieve all keys in the dict, excluding ignored keys."""
     keys = []
     for k in super(DotDict, self).keys():
-      if k not in self.excluded_vars:
+      if k not in self.attrs_not_keys:
         keys.append(k)
     return tuple(keys)
 
@@ -46,7 +70,7 @@ class DotDict(dict):
     """Retrieve all values in the dict, excluding values of ignored keys."""
     values = []
     for k, v in super(DotDict, self).items():
-      if k not in self.excluded_vars:
+      if k not in self.attrs_not_keys:
         values.append(v)
     return tuple(values)
 
@@ -54,7 +78,7 @@ class DotDict(dict):
     """Retrieve all items in the dict, excluding ignored items."""
     items = []
     for k, v in super(DotDict, self).items():
-      if k not in self.excluded_vars:
+      if k not in self.attrs_not_keys:
         items.append((k, v))
     return items
 
@@ -63,26 +87,16 @@ class DotDict(dict):
     for key in tuple(self.keys()):
       self[key] = np.asarray(self[key])
 
-  def add_excluded_var(self, *args):
-    """Add excluded variable names. """
+  def add_attr_not_key(self, *args):
+    """Add excluded attribute when retrieving dictionary keys. """
     for arg in args:
       if not isinstance(arg, str):
         raise TypeError('Only support string.')
-    self.excluded_vars += args
-
-
-def flatten_func(x: DotDict):
-  keys = []
-  values = []
-  for k, v in x.items():
-    if k not in x.excluded_vars:
-      values.append(v)
-      keys.append(k)
-  return tuple(values), tuple(keys)
+    self.attrs_not_keys += args
 
 
 register_pytree_node(
   DotDict,
-  flatten_func,
+  lambda x: (x.values(), x.keys()),
   lambda keys, values: DotDict(safe_zip(keys, values))
 )
