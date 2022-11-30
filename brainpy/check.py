daea4d4d4e865a0cc8725ae69f42658387728a91
chaoming <adaduo@outlook.com>
2022-11-30 09:50:36 +0800

    update package check in the initialization phase

--- brainpy/check.py
+++ brainpy/check.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+from brainpy.errors import PackageMissingError
+
 __all__ = [
   'is_checking',
   'turn_on',
@@ -8,6 +10,8 @@ __all__ = [
 ]
 
 _check = True
+
+_BRAINPYLIB_MINIMAL_VERSION = '0.1.2'
 
 
 def is_checking():
@@ -25,3 +29,53 @@ def turn_off():
   """Turn off the checking."""
   global _check
   _check = False
+
+
+try:
+  import jaxlib
+  del jaxlib
+except ModuleNotFoundError:
+  raise ModuleNotFoundError(
+    '''
+
+BrainPy needs jaxlib, please install it. 
+
+1. If you are using Windows system, install jaxlib through
+
+   >>> pip install jaxlib -f https://whls.blob.core.windows.net/unstable/index.html
+
+2. If you are using macOS platform, install jaxlib through
+
+   >>> pip install jaxlib -f https://storage.googleapis.com/jax-releases/jax_releases.html
+
+3. If you are using Linux platform, install jaxlib through
+
+   >>> pip install jaxlib -f https://storage.googleapis.com/jax-releases/jax_releases.html
+
+4. If you are using Linux + CUDA platform, install jaxlib through
+
+   >>> pip install jaxlib -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+Note that the versions of "jax" and "jaxlib" should be consistent, like "jax=0.3.14" and "jaxlib=0.3.14".  
+
+For more detail installation instructions, please see https://brainpy.readthedocs.io/en/latest/quickstart/installation.html#dependency-2-jax 
+
+    ''') from None
+
+try:
+  import brainpylib
+
+  if brainpylib.__version__ < _BRAINPYLIB_MINIMAL_VERSION:
+    raise PackageMissingError(
+      f'brainpy need "brainpylib>={_BRAINPYLIB_MINIMAL_VERSION}". \n'
+      f'Please install it through:\n\n'
+      f'>>> pip install brainpylib -U'
+    )
+
+  del brainpylib
+except ModuleNotFoundError:
+  raise PackageMissingError(
+    f'brainpy need "brainpylib>={_BRAINPYLIB_MINIMAL_VERSION}". \n'
+    f'Please install "brainpylib>={_BRAINPYLIB_MINIMAL_VERSION}" through:\n\n'
+    f'>>> pip install brainpylib'
+  )
