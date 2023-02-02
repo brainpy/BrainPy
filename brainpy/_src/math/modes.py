5d355a3627792b1919f297c67dd9b7639f853403
chaoming <chao.brain@qq.com>
2023-02-02 09:39:21 +0800

    enable Mode checking by itself

--- brainpy/_src/math/modes.py
+++ brainpy/_src/math/modes.py
@@ -19,6 +19,24 @@ class Mode(object):
   def __repr__(self):
     return self.__class__.__name__
 
+  def __eq__(self, other: 'Mode'):
+    assert isinstance(other, Mode)
+    return other.__class__ == self.__class__
+
+  def is_a(self, mode: type):
+    assert isinstance(mode, type)
+    return self.__class__ == mode
+  
+  def is_subclass(self, *modes):
+    cls = self.__class__
+    for smode in modes:
+      if not isinstance(smode, type):
+        raise TypeError(f'supported_types must be a tuple/list of type. But wwe got {smode}')
+    if all([not issubclass(smode, cls) for smode in modes]):
+      return False
+    else:
+      return True
+
 
 class NonBatchingMode(Mode):
   """Normal non-batching mode.
