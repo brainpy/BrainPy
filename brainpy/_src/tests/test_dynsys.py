896a75207503ba7492e57e03f5a338f590728b84
chaoming <adaduo@outlook.com>
2023-12-14 20:44:55 +0800

    [dyn] add `brainpy.reset_level()` decorator

--- brainpy/_src/tests/test_dynsys.py
+++ brainpy/_src/tests/test_dynsys.py
@@ -1,3 +1,4 @@
+import unittest
 
 import brainpy as bp
 
@@ -36,5 +37,19 @@ def test3():
   B()(1.)
 
 
+class TestResetLevelDecorator(unittest.TestCase):
+  _max_level = 10  # Define the maximum level for testing purposes
 
+  @bp.reset_level(5)
+  def test_function_with_reset_level_5(self):
+    self.assertEqual(self.test_function_with_reset_level_5.reset_level, 5)
 
+  def test1(self):
+    with self.assertRaises(ValueError):
+      @bp.reset_level(12)  # This should raise a ValueError
+      def test_function_with_invalid_reset_level(self):
+          pass  # Call the function here to trigger the ValueError
+
+  @bp.reset_level(-3)
+  def test_function_with_negative_reset_level(self):
+    self.assertEqual(self.test_function_with_negative_reset_level.reset_level, self._max_level - 3)
