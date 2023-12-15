ecb7bdea9b85ee896f72764dc8e3eb976ef75f92
chaoming <adaduo@outlook.com>
2023-12-15 15:18:18 +0800

    update

--- brainpy/_src/math/object_transform/autograd.py
+++ brainpy/_src/math/object_transform/autograd.py
@@ -6,6 +6,7 @@ from typing import Union, Callable, Dict, Sequence, Any, Optional
 
 import jax
 import numpy as np
+
 if jax.__version__ >= '0.4.16':
   from jax.extend import linear_util
 else:
@@ -15,31 +16,22 @@ from jax import dtypes, vmap, numpy as jnp, core
 from jax._src.api import (_vjp, _jvp)
 from jax.api_util import argnums_partial
 from jax.interpreters import xla
-from jax.tree_util import (
-  tree_flatten, tree_unflatten,
-  tree_map, tree_transpose,
-  tree_structure
-)
+from jax.tree_util import (tree_flatten, tree_unflatten,
+                           tree_map, tree_transpose,
+                           tree_structure)
 from jax.util import safe_map
 
 from brainpy import tools, check
 from brainpy._src.math.ndarray import Array, _as_jax_array_
-from .tools import (
-  dynvar_deprecation,
-  node_deprecation,
-  get_stack_cache,
-  cache_stack,
-)
-from .base import (
-  BrainPyObject,
-  ObjectTransform
-)
-from .variables import (
-  Variable,
-  VariableStack,
-  current_transform_number,
-  new_transform,
-)
+from .tools import (dynvar_deprecation,
+                    node_deprecation,
+                    get_stack_cache,
+                    cache_stack)
+from .base import (BrainPyObject, ObjectTransform)
+from .variables import (Variable,
+                        VariableStack,
+                        current_transform_number,
+                        new_transform)
 
 __all__ = [
   'grad',  # gradient of scalar function
@@ -467,7 +459,8 @@ def _std_basis(pytree):
   return _unravel_array_into_pytree(pytree, 1, flat_basis)
 
 
-_isleaf = lambda x: isinstance(x, Array)
+def _isleaf(x):
+  return isinstance(x, Array)
 
 
 def _jacrev(fun, argnums=0, holomorphic=False, allow_int=False, has_aux=False, return_value=False):
@@ -595,9 +588,6 @@ jacobian = jacrev
 
 def _jacfwd(fun, argnums=0, holomorphic=False, has_aux=False, return_value=False):
   _check_callable(fun)
-  if has_aux and jax.__version__ < '0.2.28':
-    raise NotImplementedError(f'"has_aux" only supported in jax>=0.2.28, but we detect '
-                              f'the current jax version is {jax.__version__}')
 
   @wraps(fun)
   def jacfun(*args, **kwargs):
