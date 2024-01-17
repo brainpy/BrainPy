015dc60d180ac698767ad9762c1b00f4cf12344f
He Sichao <1310722434@qq.com>
2024-01-17 15:16:12 +0800

    [taichi] Make taichi caches more transparent and Add clean caches function

--- brainpy/_src/math/op_register/__init__.py
+++ brainpy/_src/math/op_register/__init__.py
@@ -2,5 +2,6 @@
 from .numba_approach import (CustomOpByNumba,
                              register_op_with_numba,
                              compile_cpu_signature_with_numba)
+from .taichi_aot_based import clean_caches
 from .base import XLACustomOp
 from .utils import register_general_batching
