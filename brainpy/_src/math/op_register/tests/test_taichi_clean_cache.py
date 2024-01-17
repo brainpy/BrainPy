576e9c9d192a3dc30eb8e08efeb92fac9d9cd9f0
He Sichao <1310722434@qq.com>
2024-01-17 15:26:15 +0800

    Fix bugs

--- brainpy/_src/math/op_register/tests/test_taichi_clean_cache.py
+++ brainpy/_src/math/op_register/tests/test_taichi_clean_cache.py
@@ -1,3 +1,7 @@
 import brainpy.math as bm
 
+print(bm.check_kernels_count())
+
 bm.clean_caches()
+
+print(bm.check_kernels_count())
--- brainpy/math/op_register.py
+++ brainpy/math/op_register.py
@@ -5,6 +5,7 @@ from brainpy._src.math.op_register import (
   CustomOpByNumba,
   compile_cpu_signature_with_numba,
   clean_caches,
+  check_kernels_count,
 )
 
 from brainpy._src.math.op_register.base import XLACustomOp
