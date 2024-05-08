67be1de9c297401b529a3b9d106473d00ec88ae9
He Sichao <1310722434@qq.com>
2024-05-08 13:39:05 +0800

    [math] Update `CustomOpByNumba` to support JAX version >= 0.4.24

--- brainpy/_src/math/op_register/__init__.py
+++ brainpy/_src/math/op_register/__init__.py
@@ -1,5 +1,5 @@
 from .numba_approach import (CustomOpByNumba,
-                             register_op_with_numba,
+                             register_op_with_numba_xla,
                              compile_cpu_signature_with_numba)
 from .base import XLACustomOp
 from .utils import register_general_batching
