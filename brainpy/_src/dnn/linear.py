939f03024c60c349f24df413fdc0b1ffee022ac1
He Sichao <1310722434@qq.com>
2024-06-01 22:06:13 +0800

    Update linear.py

--- brainpy/_src/dnn/linear.py
+++ brainpy/_src/dnn/linear.py
@@ -987,7 +987,7 @@ class JitLinear(Layer):
 
 class JitFPHomoLayer(JitLinear):
   def get_conn_matrix(self):
-    return bm.jitconn.get_uniform_weight_matrix(self.weight, self.prob, self.seed,
+    return bm.jitconn.get_homo_weight_matrix(self.weight, self.prob, self.seed,
                                                 shape=(self.num_out, self.num_in),
                                                 transpose=self.transpose,
                                                 outdim_parallel=not self.atomic)
