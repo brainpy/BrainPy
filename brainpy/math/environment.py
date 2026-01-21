2995effa8aaa4812dde3821d43f74c28a7c75460
Chaoming Wang <adaduo@outlook.com>
2026-01-21 17:11:24 +0800

    fix: update backend import for compatibility with jax>=0.8.0

--- brainpy/math/environment.py
+++ brainpy/math/environment.py
@@ -25,7 +25,6 @@ from typing import Any, Callable, TypeVar, cast
 import brainstate.environ
 import jax
 from jax import config, numpy as jnp, devices
-from jax.lib import xla_bridge
 
 from . import modes
 from . import scales
@@ -733,8 +732,13 @@ def clear_buffer_memory(
       Clear name cache. Default is True.
 
     """
+    if jax.__version_info__ < (0, 8, 0):
+        from jax.lib.xla_bridge import get_backend
+    else:
+        from jax.extend.backend import get_backend
+
     if array:
-        for buf in xla_bridge.get_backend(platform).live_buffers():
+        for buf in get_backend(platform).live_buffers():
             buf.delete()
     if compilation:
         jax.clear_caches()
