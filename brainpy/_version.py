6abc7adccc68001efbdc3f4ea82e452f59269aa0
Chaoming Wang <chao.brain@qq.com>
2026-07-09 13:30:51 +0800

    chore(release): 2.8.1 (#869)
    Bump version to 2.8.1 and add the 2.8.1 changelog entry.

--- brainpy/_version.py
+++ brainpy/_version.py
@@ -14,5 +14,5 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = "2.8.0"
+__version__ = "2.8.1"
 __version_info__ = tuple(map(int, __version__.split(".")))
